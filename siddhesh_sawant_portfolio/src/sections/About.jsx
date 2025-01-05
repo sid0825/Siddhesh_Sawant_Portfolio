10aa01f5071419363a5dc7c1582ac6386e46d06a
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2025-01-04 20:01:03 -0500

    Added About Section

---
+++ siddhesh_sawant_portfolio/src/sections/About.jsx
@@ -0,0 +1,119 @@
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
+
+import Button from '../components/Button.jsx';
+
+const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <section className="c-space my-20" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Hi, I’m Siddhesh Sawant</p>
+              <p className="grid-subtext">
+                With 2.9 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
+                and responsive websites.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
+                applications <br />
+                <br />
+                <b>Programming Languages:</b> <i>Java, JavaScript, TypeScript, Python</i><br />
+                <br />
+                <b>Databases:</b> <i>MySQL, MongoDB</i><br />
+                <br />
+                <b>Technologies:</b> <i>HTML, CSS, Spring, Spring-Boot, Express, NodeJS, ReactJS, Redux, JSON, RESTful APIs, Swing, XML</i><br />
+                <br />
+                <b>Developer Tools:</b> <i>Eclipse, VScode, PostMan, NetBeans, Vite</i><br />
+                <br />
+                <b>Version Controls:</b> <i>GitHub, Tortoise SVN</i>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
+              <p className="grid-subtext">I&apos;m based in United States, Boston and open to remote work worldwide.</p>
+              <a href="https://drive.google.com/drive/u/0/folders/1YdfmybAqFy2lbLZu3rDt79AWgG6V04X9" download target="_blank" rel="noopener noreferrer">
+              <Button name="Download Resume" isBeam containerClass="w-full mt-10" />
+              </a>
+
+
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I thrive on solving complex problems and creating impactful solutions through code. Programming is more than a profession—it's my passion. I enjoy building robust systems, exploring emerging technologies, and continuously enhancing my skills to deliver innovative results.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">siddheshlsawant@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
